3440665f9f5e1bef587cb2d4003c650f1fcb8c18
Kjell Erik Reed Anda <keaaa@statoil.com>
2019-12-10 21:32:49 +0100

    added general and body

--- main.go
+++ main.go
@@ -2,24 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Method %q, Request %q\n", r.Method, r.RequestURI)
-	fmt.Printf("Method %q, Request %q\n", r.Method, r.RequestURI)
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "General\n")
+	fmt.Fprintf(w, "Request URL: %q\n", r.RequestURI)
+	fmt.Fprintf(w, "Request Method: %q\n", r.Method)
+	fmt.Fprintf(w, "Remote Address: %q\n", r.RemoteAddr)
 
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "Request Headers\n")
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(body) > 0 {
+		fmt.Fprintf(w, "\n")
+		fmt.Fprintf(w, "Body\n")
+		fmt.Fprintf(w, "%s", string(body))
 	}
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.PathPrefix("/").HandlerFunc(homeLink)
-	log.Printf("http-echo listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.PathPrefix("/").HandlerFunc(defaultHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("http-echo listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
